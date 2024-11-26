4afb2261b5cc1fe77659691d991a4ff83c304888
Sean <86182778+auroraongithub@users.noreply.github.com>
2024-11-26 16:29:56 +0800

    Update script.js

--- script.js
+++ script.js
@@ -233,37 +233,39 @@ updateConversionFields();
 /* ASSIGNMENT 2 */
 
 function calculateTax() {
-  let ti, basictax, brackettax, totaltax;
+
+let ti, basictax, brackettax, totaltax;
 
   ti = document.getElementById("taxableincome").value * 1;
 
-  if (ti < 250000) {
+  if (ti <= 250000) {
       basictax = 0;
       brackettax = 0;
-  } else if (ti >= 250000 && ti < 400000) {
+  } else if (ti > 250000 && ti <= 400000) {
       basictax = 0;
       brackettax = 0.2 * (ti - 250000);
-  } else if (ti >= 400000 && ti < 800000) {
+  } else if (ti >= 400000 && ti <= 800000) {
       basictax = 30000;
       brackettax = 0.25 * (ti - 400000);
-  } else if (ti >= 800000 && ti < 2000000) {
+  } else if (ti >= 800000 && ti <= 2000000) {
       basictax = 130000;
       brackettax = 0.30 * (ti - 800000);
-  } else if (ti >= 2000000 && ti < 8000000) {
+  } else if (ti >= 2000000 && ti <= 8000000) {
       basictax = 490000;
       brackettax = 0.32 * (ti - 2000000);
-  } else if (ti >= 8000000) {
+  } else {
       basictax = 2410000;
       brackettax = 0.35 * (ti - 8000000);
   }
 
   totaltax = basictax + brackettax;
-  document.getElementById("incometax").textContent = `Income Tax: ${totaltax.toFixed(0)}`; 
+  document.getElementById("incometax").textContent = `Income Tax: ${totaltax.toFixed(2)}`; 
 }
 
 /* ASSIGNMENT 3 */
 
 function numberCalculator() {
+
   const n = parseInt(document.getElementById("numberInput").value);
   const operation = document.getElementById("operation").value;
   let resultText = "";
