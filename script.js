c1c55257d6c2887905793a360ebb4f00c33f285a
Sean <86182778+auroraongithub@users.noreply.github.com>
2024-11-26 16:36:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -234,32 +234,62 @@ updateConversionFields();
 
 function calculateTax() {
 
-let ti, basictax, brackettax, totaltax;
+  let userNum = 0, tax = 0;
+
+ti = document.getElementById("taxableincome").value*1;
+
+console.log(userNum);
+
+            if(ti <= 250000)
+            {
+                basictax = 0;
+            }
+            else if(userNum > 250000 && userNum <= 400000)
+            {
+                tax = (userNum-250000)*0.20;
+            }
+            else if(userNum > 400000 && userNum <= 800000)
+            {
+                tax = 30000 + ((userNum-400000)*0.25);
+            }
+            else if(userNum > 800000 && userNum <= 2000000)
+            {
+                tax = 130000 + ((userNum-800000)*0.30);
+            }
+            else if (userNum > 2000000 && userNum <= 8000000)
+            {
+                tax = 490000 + ((userNum-2000000)*0.32);
+            }
+            else
+            {
+                tax = 2410000 + ((userNum-8000000)*0.35);
+            }
+
+            tax = tax.toFixed(2);
+            document.getElementById("taxedIncome").value = tax;
+        }
+  
+
+let ti, basictax, totaltax;
 
   ti = document.getElementById("taxableincome").value * 1;
 
   if (ti <= 250000) {
       basictax = 0;
-      brackettax = 0;
   } else if (ti > 250000 && ti <= 400000) {
-      basictax = 0;
-      brackettax = 0.2 * (ti - 250000);
-  } else if (ti >= 400000 && ti <= 800000) {
-      basictax = 30000;
-      brackettax = 0.25 * (ti - 400000);
-  } else if (ti >= 800000 && ti <= 2000000) {
-      basictax = 130000;
-      brackettax = 0.30 * (ti - 800000);
-  } else if (ti >= 2000000 && ti <= 8000000) {
-      basictax = 490000;
-      brackettax = 0.32 * (ti - 2000000);
+      totaltax = (ti-250000) * 0.20;
+  } else if (ti > 400000 && ti <= 800000) {
+      totaltax = 30000 + ((ti-400000) * 0.25);
+  } else if (ti > 800000 && ti <= 2000000) {
+      totaltax = 130000 + ((ti-800000) * 0.30);
+  } else if (ti > 2000000 && ti <= 8000000) {
+      totaltax = 490000 + ((ti-2000000) * 0.32);
   } else {
-      basictax = 2410000;
-      brackettax = 0.35 * (ti - 8000000);
+      totaltax = 2410000 + ((ti-8000000) * 0.35);
   }
 
-  totaltax = basictax + brackettax;
-  document.getElementById("incometax").textContent = `Income Tax: ${totaltax.toFixed(2)}`; 
+  totaltax = totaltax.toFixed(2);
+  document.getElementById("incometax").textContent = `Income Tax: ${totaltax}`; 
 }
 
 /* ASSIGNMENT 3 */
