c070408b23732d9655c4bb11410a7ca7514d14a9
Sean <86182778+auroraongithub@users.noreply.github.com>
2024-11-26 16:37:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -234,42 +234,6 @@ updateConversionFields();
 
 function calculateTax() {
 
-  let userNum = 0, tax = 0;
-
-ti = document.getElementById("taxableincome").value*1;
-
-console.log(userNum);
-
-            if(ti <= 250000)
-            {
-                basictax = 0;
-            }
-            else if(userNum > 250000 && userNum <= 400000)
-            {
-                tax = (userNum-250000)*0.20;
-            }
-            else if(userNum > 400000 && userNum <= 800000)
-            {
-                tax = 30000 + ((userNum-400000)*0.25);
-            }
-            else if(userNum > 800000 && userNum <= 2000000)
-            {
-                tax = 130000 + ((userNum-800000)*0.30);
-            }
-            else if (userNum > 2000000 && userNum <= 8000000)
-            {
-                tax = 490000 + ((userNum-2000000)*0.32);
-            }
-            else
-            {
-                tax = 2410000 + ((userNum-8000000)*0.35);
-            }
-
-            tax = tax.toFixed(2);
-            document.getElementById("taxedIncome").value = tax;
-        }
-  
-
 let ti, basictax, totaltax;
 
   ti = document.getElementById("taxableincome").value * 1;
